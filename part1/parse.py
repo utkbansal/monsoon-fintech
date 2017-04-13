d6605118718d5cfe445b78ac6aa884cecf29b9ea
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2017-04-13 16:25:28 +0530

    Refactor PDFToCSVConverter class

--- part1/parse.py
+++ part1/parse.py
@@ -2,38 +2,6 @@ import pandas
 from tabula import read_pdf_table
 
 
-def get_amount(value):
-    if value is None:
-        return ""
-    elif type(value) is float:
-        return value
-    else:
-        return value.split(" ")[0]
-
-
-def get_text(value):
-    if pandas.isnull(value):
-        return ""
-    else:
-        return " ".join(value.split(" ", 1)[1:])
-
-
-df = read_pdf_table('/Users/utkbansal/Code/MonsoonFintech/part1/data/Balsheet.pdf')
-
-print(list(df.columns.values))
-df.dropna(axis='columns', how='all', inplace=True)
-df.rename(columns={'Unnamed: 4': ''}, inplace=True)
-# df['2016x'], df['Particlarsx'] = df['2016 Particulars'].apply(splitter)
-df['2016'] = df['2016 Particulars'].apply(get_amount)
-df['Particulars.1'] = df['2016 Particulars'].apply(get_text)
-print(list(df.columns.values))
-print(df)
-df.to_csv('out.csv')
-
-
-# convert_into('/Users/utkbansal/Code/MonsoonFintech/part1/data/Balsheet.pdf', 'direct.csv', output_format='csv')
-
-
 class PDFToCSVConverter(object):
     def __init__(self, path):
         self.path = path
@@ -45,17 +13,38 @@ class PDFToCSVConverter(object):
 
     def fix_columns_names(self):
         # If the column doesn't have a name it should be left blank
-        pass
+        self.data_frame.rename(columns={'Unnamed: 4': ''}, inplace=True)
 
     def split_columns(self):
         # Split columns that are clubbed together
-        pass
+        self.data_frame['2016.1'] = self.data_frame['2016 Particulars'].apply(self.get_amount)
+        self.data_frame['Particulars.1'] = self.data_frame['2016 Particulars'].apply(self.get_text)
 
-    def write(self, name='outfile.csv'):
+    def write(self, name='data/outfile.csv'):
+        self.remove_blank_columns()
+        self.fix_columns_names()
+        self.split_columns()
         self.data_frame.to_csv(name)
+
+    @staticmethod
+    def get_amount(value):
+        if value is None:
+            return ""
+        elif type(value) is float:
+            return value
+        else:
+            return value.split(" ")[0]
+
+    @staticmethod
+    def get_text(value):
+        if pandas.isnull(value):
+            return ""
+        else:
+            return " ".join(value.split(" ", 1)[1:])
 
 
 if __name__ == '__main__':
-    path = input("Enter the absolute path to the pdf file")
+    # path = input("Enter the absolute path to the pdf file")
+    path = '/Users/utkbansal/Code/MonsoonFintech/part1/data/Balsheet.pdf'
     converter = PDFToCSVConverter(path)
     converter.write()

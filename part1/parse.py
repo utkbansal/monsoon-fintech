e25f9b8ecacb89a15187e061769179c601b7b504
Bansal Utkarsh <bansalutkarsh3@gmail.com>
2017-04-13 14:31:48 +0530

    Creates basic skeleton of PDFToCSVConverter

--- part1/parse.py
+++ part1/parse.py
@@ -10,6 +10,7 @@ def get_amount(value):
     else:
         return value.split(" ")[0]
 
+
 def get_text(value):
     if pandas.isnull(value):
         return ""
@@ -18,7 +19,6 @@ def get_text(value):
 
 
 df = read_pdf_table('/Users/utkbansal/Code/MonsoonFintech/part1/data/Balsheet.pdf')
-
 
 print(list(df.columns.values))
 df.dropna(axis='columns', how='all', inplace=True)
@@ -30,4 +30,32 @@ print(list(df.columns.values))
 print(df)
 df.to_csv('out.csv')
 
+
 # convert_into('/Users/utkbansal/Code/MonsoonFintech/part1/data/Balsheet.pdf', 'direct.csv', output_format='csv')
+
+
+class PDFToCSVConverter(object):
+    def __init__(self, path):
+        self.path = path
+        # TODO: Check for existence of the file and permissions to read it
+        self.data_frame = read_pdf_table(path)
+
+    def remove_blank_columns(self):
+        self.data_frame.dropna(axis='columns', how='all', inplace=True)
+
+    def fix_columns_names(self):
+        # If the column doesn't have a name it should be left blank
+        pass
+
+    def split_columns(self):
+        # Split columns that are clubbed together
+        pass
+
+    def write(self, name='outfile.csv'):
+        self.data_frame.to_csv(name)
+
+
+if __name__ == '__main__':
+    path = input("Enter the absolute path to the pdf file")
+    converter = PDFToCSVConverter(path)
+    converter.write()
